183155a4956f0bff7e486835f86c9ab8d26d5a17
WilboS <27775037+WilboS@users.noreply.github.com>
2017-10-04 17:29:28 +0100

    Add files via upload

--- LeapYear.java
+++ LeapYear.java
@@ -1,22 +1,21 @@
+//A leap year is a year divisible by 4, unless it is also divisible by 100 but not 400.
+
 public class LeapYear{
 	public static void main (String args[]){
-
 
 		int year = Integer.parseInt(args[0]);
 
 
-		if (year % 4 == 0)
-			System.out.println("Leap Year");
-
-		if (year % 400 == 0)
-			System.out.println("Leap Year");
-
-		if (year % 100 == 0)
+		if (year % 400 != 0 && year % 100 == 0)
 			System.out.println("Not a Leap Year");
+
+		else if (year % 4 == 0)
+			System.out.println("Leap Year");
+
+		else 
+			System.out.println("Not a leap year");
 		
-		else
-			System.out.println("Not a Leap Year");
-
 
 	}
 }
+
